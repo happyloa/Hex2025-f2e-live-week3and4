62df12b0b0ba85416f3786192f60b41e340a0849
Aaron <piafyoyo06@gmail.com>
2025-07-05 07:27:12 +0800

    feat: 新增首頁

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- Banner -->
+  <section
+    class="h-[480px] content-center bg-[url(/images/mobile/home/banner.webp)] bg-cover bg-center px-6 md:h-[800px] md:bg-[url(/images/desktop/home/banner.webp)]"
+  >
+    <div class="mx-auto max-w-[1520px]">
+      <div class="mb-8 font-bold text-white md:mb-12">
+        <h1 class="mb-2 text-display-2 md:mb-4 md:text-display-1">URBNSTEP</h1>
+        <p class="text-h5 md:text-h3">Your Step, Your Statement</p>
+      </div>
+      <AtomButton text="立即選購" />
+    </div>
+  </section>
+</template>

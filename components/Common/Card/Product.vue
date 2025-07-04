3f53ebc22bd143bab442a2fab4428210c28ea055
Aaron <piafyoyo06@gmail.com>
2025-07-05 07:56:40 +0800

    feat: 帶名稱與價格的商品卡片

---
+++ components/Common/Card/Product.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+  /**
+   * 商品圖片路徑
+   * @type {String}
+   */
+  img: {
+    type: String,
+    default: "/images/desktop/home/new-1.webp",
+  },
+  /**
+   * 商品名稱
+   * @type {String}
+   */
+  name: {
+    type: String,
+    default: "透過 name prop 來輸入商品名稱",
+  },
+  /**
+   * 商品價格
+   * @type {String}
+   */
+  price: {
+    type: String,
+    default: "透過 price prop 來輸入商品價格",
+  },
+});
+const { img, name, price } = toRefs(props);
+</script>
+
+<template>
+  <article class="group">
+    <img
+      :src="img"
+      :alt="name"
+      class="mb-2 block transition duration-300 group-hover:opacity-80 sm:mb-3"
+    />
+    <h3 class="mb-0 text-h6 font-bold sm:mb-1 sm:text-h5">{{ name }}</h3>
+    <span class="text-label-md font-medium">NT ${{ price }}</span>
+  </article>
+</template>

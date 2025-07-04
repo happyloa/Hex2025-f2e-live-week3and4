71a3cd236b6e54bb8dd3c309eb7a935230122a39
Aaron <piafyoyo06@gmail.com>
2025-07-05 07:30:39 +0800

    feat: 連結到商品列表頁

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
         <h1 class="mb-2 text-display-2 md:mb-4 md:text-display-1">URBNSTEP</h1>
         <p class="text-h5 md:text-h3">Your Step, Your Statement</p>
       </div>
-      <AtomButton text="立即選購" />
+      <AtomButton to="/shop" text="立即選購" />
     </div>
   </section>
 </template>

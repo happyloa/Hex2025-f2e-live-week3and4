de4135a0659823037fbe580f857f675d09e8c2e1
Aaron <piafyoyo06@gmail.com>
2025-07-05 22:21:55 +0800

    feat: 引入導覽選單

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="font-bold text-neutral selection:bg-primary selection:text-white">
     <NuxtRouteAnnouncer />
+    <LayoutHeader />
     <NuxtPage />
     <LayoutFooter />
   </div>

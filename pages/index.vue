5e51c79544a4b4f71880940f3831af1a7bb5a7e7
Aaron <piafyoyo06@gmail.com>
2025-07-05 08:01:57 +0800

    feat: 新品上市區塊

--- pages/index.vue
+++ pages/index.vue
@@ -11,4 +11,43 @@
       <AtomButton to="/shop" text="立即選購" />
     </div>
   </section>
+  <!-- 新品上市 -->
+  <section class="px-3 py-10 md:py-16">
+    <div class="mx-auto max-w-container">
+      <div class="mb-6 text-center font-bold md:mb-8">
+        <h2 class="mb-2 text-h2 md:mb-3 md:text-display-2">New Arrival</h2>
+        <p class="text-h4 md:text-h2">新品上市</p>
+      </div>
+      <ul class="grid grid-cols-2 gap-x-3 gap-y-6 md:gap-x-6 md:gap-y-10">
+        <li>
+          <CommonCardProduct
+            img="/images/desktop/home/new-1.webp"
+            name="Monoverse"
+            price="3,600"
+          />
+        </li>
+        <li>
+          <CommonCardProduct
+            img="/images/desktop/home/new-2.webp"
+            name="PLATFORM 404"
+            price="2,600"
+          />
+        </li>
+        <li>
+          <CommonCardProduct
+            img="/images/desktop/home/new-3.webp"
+            name="URBN Force"
+            price="3,200"
+          />
+        </li>
+        <li>
+          <CommonCardProduct
+            img="/images/desktop/home/new-4.webp"
+            name="Mode90"
+            price="4,000"
+          />
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>

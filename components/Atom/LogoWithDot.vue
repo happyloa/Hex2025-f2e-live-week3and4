4dc3e7820748a58cb32df13ed763e4d70ca2264d
Aaron <piafyoyo06@gmail.com>
2025-07-05 20:33:17 +0800

    feat: 新增帶有點點的 logo 元件

---
+++ components/Atom/LogoWithDot.vue
@@ -0,0 +1,8 @@
+<template>
+  <NuxtLink to="/" class="relative z-10 self-start px-3 py-2">
+    <span class="mina | relative z-10 text-label-lg">URBNSTEP</span>
+    <div
+      class="absolute right-[11px] top-[11px] z-0 size-2 rounded-full bg-primary-300"
+    ></div>
+  </NuxtLink>
+</template>

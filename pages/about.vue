7270b364181f5e5ee53a146030d39aee656a1fd3
Aaron <piafyoyo06@gmail.com>
2025-07-06 11:09:43 +0800

    feat: 品牌故事頁

--- pages/about.vue
+++ pages/about.vue
@@ -1,1 +1,172 @@
-<template><h1>品牌故事頁</h1></template>
+<script setup>
+useSeoMeta({
+  title: "品牌故事 | 2025 切版直播班 - 鞋子電商 W3&4",
+  ogTitle: "品牌故事 | 2025 切版直播班 - 鞋子電商 W3&4",
+});
+</script>
+
+<template>
+  <!-- Hero 區塊 -->
+  <section
+    class="h-[240px] content-center bg-[url(/images/desktop/about/hero.webp)] bg-cover bg-center text-center text-white md:h-[400px]"
+  >
+    <h1 class="mb-2 text-h3 md:mb-4 md:text-h1">品牌故事</h1>
+    <p class="text-h5 md:text-h3">Brand Story</p>
+  </section>
+  <!-- 內容 -->
+  <section class="px-3 py-8 md:py-16">
+    <!-- 理念 -->
+    <div
+      class="mx-auto mb-8 max-w-[856px] rounded bg-primary-100 p-6 text-start md:mb-12 md:p-8 md:text-center"
+    >
+      <h2 class="mb-3 text-h5 text-primary">
+        走出城市的節奏，穿上自己的步調。
+      </h2>
+      <p class="mb-1 text-paragraph-md font-normal">
+        我們相信，每一雙鞋都該帶著個性出場。
+      </p>
+      <p class="mb-1 text-paragraph-md font-normal">
+        在 URBNSTEP，我們認為鞋履不只是穿戴飾品，更是你與世界連結的語言。
+      </p>
+      <p class="text-paragraph-md font-normal">
+        融合城市結構與機能語彙，我們打造適合都會節奏的風格鞋款，陪你自信穿梭每一場風景。
+      </p>
+    </div>
+    <!-- 品牌簡介 -->
+    <div class="mx-auto mb-8 max-w-[1076px] md:mb-12">
+      <img
+        src="/public/images/desktop/about/about.webp"
+        alt="品牌故事大圖"
+        class="mb-4 md:mb-6"
+      />
+      <div class="mx-auto max-w-[856px]">
+        <h2 class="mb-4 text-h5 md:mb-6 md:text-h4">品牌簡介</h2>
+        <div class="border-b border-neutral-300 pb-8 md:pb-12">
+          <h3 class="mb-3 text-h6 text-primary md:mb-4 md:text-h5">
+            URBNSTEP
+            是來自台灣的街頭機能鞋品牌，致力於打造「風格與功能並存」的鞋履體驗。
+          </h3>
+          <p class="mb-3 text-paragraph-md font-normal md:mb-4">
+            在城市與日常的交錯中，我們看見了步伐的力量。URBNSTEP
+            誕生於一群設計師與文化觀察者的交會點——一個相信風格應該來自「真實場景」而非伸展台的團隊。從都市的清晨街角，到午後光影灑落的人行道，我們捕捉的是那些不被刻意編排的時刻，那些用一雙鞋踏出的態度與節奏。每一款鞋的誕生，不只是設計與工藝的結晶，更是一段來自城市靈感的延伸，我們相信：鞋，是穿在腳上的語言。
+          </p>
+          <p class="text-paragraph-md font-normal">
+            URBNSTEP 的名字來自 Urban +
+            Step，代表著我們對每一個走進城市生活的人致敬。我們不追逐潮流，我們關注輪廓、材質、穿著者的心境與身型，創造能陪伴你走進自我節奏的經典之作。選用高品質麂皮、織物與天然橡膠大底，並與獨立插畫家、攝影師合作，打造具有街頭詩意的形象風格。URBNSTEP
+            相信，美不是炫耀，而是由內而外的呼吸——一雙鞋，剛好說出你心中的樣子。
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- 品牌歷程 -->
+    <div
+      class="mx-auto mb-8 max-w-[856px] border-b border-neutral-300 pb-8 md:mb-12 md:pb-12"
+    >
+      <h2 class="mb-4 text-h5 md:mb-6 md:text-h4">品牌歷程</h2>
+      <ul
+        class="flex list-inside list-disc flex-col gap-3 pl-3 text-paragraph-md font-normal"
+      >
+        <li>
+          <time datetime="2019" class="pr-2">2019</time
+          ><span class="border-l border-neutral pl-2"
+            >URBNSTEP 在台北東區的創意工作室誕生。</span
+          >
+        </li>
+        <li>
+          <time datetime="2021" class="pr-2">2021</time
+          ><span class="border-l border-neutral pl-2"
+            >首款【Urban Flow 系列】誕生，結合針織鞋面與城市靈感。</span
+          >
+        </li>
+        <li>
+          <time datetime="2025" class="pr-2">2025</time
+          ><span class="border-l border-neutral pl-2"
+            >URBNSTEP 已成為台灣街頭生活風格的代表鞋履品牌之一。</span
+          >
+        </li>
+      </ul>
+    </div>
+    <!-- 品牌理念 -->
+    <div
+      class="mx-auto mb-8 max-w-[856px] border-b border-neutral-300 pb-8 md:mb-12 md:pb-12"
+    >
+      <h2 class="mb-4 text-h5 md:mb-6 md:text-h4">品牌理念</h2>
+      <ul class="flex flex-col gap-3 text-paragraph-md font-normal">
+        <li class="border-l-[3px] border-neutral-300 pl-3">
+          機能先行：輕量、透氣、防潑水
+        </li>
+        <li class="border-l-[3px] border-neutral-300 pl-3">
+          美學延伸：極簡線條 + 實驗配色
+        </li>
+        <li class="border-l-[3px] border-neutral-300 pl-3">
+          永續材料：選用環保合成皮革與可回收包裝
+        </li>
+      </ul>
+    </div>
+    <!-- 品牌小故事 -->
+    <div
+      class="mx-auto max-w-[856px] border-b border-neutral-300 pb-8 md:pb-12"
+    >
+      <h2 class="mb-4 text-h5 md:mb-6 md:text-h4">品牌小故事</h2>
+      <ul class="text-label-md font-medium text-primary">
+        <li class="flex items-center gap-2 py-2">
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M17.5 3.125a.625.625 0 0 0-.624-.625h-7.5a.625.625 0 0 0 0 1.25h5.991L2.683 16.433a.626.626 0 1 0 .885.884L16.251 4.634v5.991a.625.625 0 1 0 1.25 0z"
+              fill="currentColor"
+            />
+          </svg>
+          <a href="#" class="border-b border-primary pb-1"
+            >創辦人筆記：我們為什麼想做鞋？</a
+          >
+        </li>
+        <li class="flex items-center gap-2 py-2">
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M17.5 3.125a.625.625 0 0 0-.624-.625h-7.5a.625.625 0 0 0 0 1.25h5.991L2.683 16.433a.626.626 0 1 0 .885.884L16.251 4.634v5.991a.625.625 0 1 0 1.25 0z"
+              fill="currentColor"
+            />
+          </svg>
+          <a href="#" class="border-b border-primary pb-1"
+            >穿上 URBNSTEP 的他們</a
+          >
+        </li>
+        <li class="flex items-center gap-2 py-2">
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M17.5 3.125a.625.625 0 0 0-.624-.625h-7.5a.625.625 0 0 0 0 1.25h5.991L2.683 16.433a.626.626 0 1 0 .885.884L16.251 4.634v5.991a.625.625 0 1 0 1.25 0z"
+              fill="currentColor"
+            />
+          </svg>
+          <a href="#" class="border-b border-primary pb-1"
+            >URBNSTEP 鞋履的誕生過程</a
+          >
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
